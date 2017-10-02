ad79523bef5fc7a1cd072831f5a43766289f3409
haileysil <hsilveira1@mail.csuchico.edu>
2017-10-02 16:53:24 -0700

    functional decomposition

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//Project 2 Functional Decomposition
+
+#include <iostream>
+#include <string>
+#include <vector>
+// #include "bitmap.h"
+
+using namespace std;
+
+int main()
+{
+
+  /*
+
+  -ask user for file name of an image
+  -convert image so every pixel is greyscale
+    -test to make sure the picture has been saved
+    -convert image to a vector of vectors, i.e. a matrix, of pixels
+    -for the pixel located at [0][0], find the integer values of the 3 components R/G/B
+    -find the average of the three values
+    -assign all 3 components to that average value
+    -repeat as long as the counter is less than the number of elements in that row
+    -repeat for every row and element in each row
+   
+   */
+ 
+// after code has compiled successfully:
+    //update/save file with different name
+    // add, commit, push onto github
+
+cout<<"You're gonna ace this thing."<<endl;
+
+        return 0;
+}
