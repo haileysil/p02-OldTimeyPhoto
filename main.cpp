59107b08eba025ad38e97d5bc196b5d75d2d1f7f
haileysil <hsilveira1@mail.csuchico.edu>
2017-10-11 16:30:16 -0700

    converts image to matrix of pixels, takes average of rgb values

--- main.cpp
+++ main.cpp
@@ -10,35 +10,64 @@ using namespace std;
 
 int main()
 {
-/*
+        /*
 
-  -ask user for file name of an image
-  -convert image so every pixel is greyscale
-    -test to make sure the picture has been saved
-    -convert image to a vector of vectors, i.e. a matrix, of pixels
-    -for the pixel located at [0][0], find the integer values of the 3 components R/G/B
-    -find the average of the three values
-    -assign all 3 components to that average value
-    -repeat as long as the counter is less than the number of elements in that row
-    -repeat for every row and element in each row
-   
-   */
- 
-// after code has compiled successfully:
-    //update/save file with different name
-    // add, commit, push onto github
+           -ask user for file name of an image
+           -convert image so every pixel is greyscale
+           -test to make sure the picture has been saved
+           -convert image to a vector of vectors, i.e. a matrix, of pixels
+           -for the pixel located at [0][0], find the integer values of the 3 components R/G/B
+           -find the average of the three values
+           -assign all 3 components to that average value
+           -repeat as long as the counter is less than the number of elements in that row
+           -repeat for every row and element in each row
 
-Bitmap image;
-vector <vector <Pixel> > bmp;
-Pixel rgb;
+         */
 
-image.open("machupicchu.bmp");
-bmp = image.toPixelMatrix();
+        // after code has compiled successfully:
+        //update/save file with different name
+        // add, commit, push onto github
 
-int numRows = bmp.size();
-int numCols = bmp[0].size();
+        Bitmap image;
+        vector <vector <Pixel> > bmp;
+        Pixel rgb;
 
-cout<<"machupicchu.bmp has been loaded. It is "<<numCols<<" pixels wide and "<<numRows<<" pixels high."<<endl;
+        image.open("machupicchu.bmp");
 
-return 0;
+        /*
+           int numRows = bmp.size();
+           int numCols = bmp[0].size();
+
+           cout<<"machupicchu.bmp has been loaded. It is "<<numCols<<" pixels wide and "<<numRows<<" pixels high."<<endl;
+         */
+
+        bool validBmp = image.isImage();
+
+        if ( validBmp == true )
+        {
+                bmp = image.toPixelMatrix();
+
+                for (int r = 0; r < bmp.size(); r++)
+                {
+                        for (int c = 0; c < bmp[r].size(); c++)
+                        {
+                        rgb = bmp[r][c];
+
+                        //convert pixels to black and white
+                        int avgColor = (rgb.red + rgb.green + rgb.blue)/3;
+                        rgb.red = avgColor;
+                        rgb.green = avgColor;
+                        rgb.blue = avgColor;
+                        bmp[r][c] = rgb; 
+                        }
+
+                }
+//        image.fromPixelMatrix(bmp);
+//        image.save("machupicchu.bmp");
+        }
+        else 
+        {
+                cout<<"This file is not a valid image. Please try again or submit a new image."<<endl;
+        }
+        return 0;
 }
